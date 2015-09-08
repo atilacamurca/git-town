9eb8076e13576071f793816ccc594e23ed52b175
allonsy <linuxbash8@gmail.com>
2015-09-08 21:53:07 +0000

    Update package files

--- package/debian_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-mkdir -pv usr/lib/git-core &&
-mkdir -pv usr/share/man/man1 &&
-cp -r ../src/* usr/lib/git-core &&
-cp -r ../man/man1 /usr/share/man/man1 &&
-debuild -us -uc &&
-rm -rf usr
-
